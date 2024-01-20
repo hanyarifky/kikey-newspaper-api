d71683bf85522d746bd8c0b3eb5ba8518ca402ee
hanyarifky <rifkydoang2014@gmail.com>
2024-01-20 15:16:47 +0700

    setting port

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import express from 'express';
 import 'dotenv/config';
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World');
+});
+
+app.post('/', (req, res) => {
+  res.send(`${req.method} is used`);
+});
+
+app.put('/', (req, res) => {
+  res.send(`${req.method} is used`);
+  const method = req.method;
+  console.log(method);
+});
+
+app.delete('/', (req, res) => {
+  res.send(`${req.method} is used`);
 });
 
 app.listen(port, 'localhost', () => {
